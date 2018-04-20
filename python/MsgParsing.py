06ed4179d68905e3c95b22bf0b4e427d636f163f
vlon <viktor.lonnroth@ha.ax>
2018-04-20 15:45:59 +0300

    Start of implementation of MCP2515 in python

---
+++ python/MsgParsing.py
@@ -0,0 +1,15 @@
+
+
+class CanMsg ():
+    
+    
+    def __init__(self):
+        self.id = 0
+        self.ide = 0
+        self.length = 0
+        self.data = []
+        for i in range (8):
+            data.append(0)
+    
+    
+            
